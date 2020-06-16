7b5bb7cb1875533085bf8772314a775f2d83c916
paulonteri <onteripaul@gmail.com>
2020-06-16 14:57:20 +0300

    Added Header

---
+++ public/index.js
@@ -0,0 +1,19 @@
+// When the user scrolls the page, execute headerPosFunction
+window.onscroll = function () {
+  headerPosFunction();
+};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function headerPosFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
