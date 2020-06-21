c1db5e5e728928aa411d35afccc88c0bdcea9813
paulonteri <onteripaul@gmail.com>
2020-06-21 15:44:39 +0300

    remove fixed div

--- public/js/index.js
+++ public/js/index.js
@@ -4,20 +4,20 @@ window.onload = function () {
     headerPosFunction();
   };
 
-  // Get the header
-  var header = document.getElementById("myHeader");
+  // // Get the header
+  // var header = document.getElementById("myHeader");
 
-  // Get the offset position of the navbar
-  var sticky = header.offsetTop;
+  // // Get the offset position of the navbar
+  // var sticky = header.offsetTop;
 
-  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  function headerPosFunction() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  }
+  // // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  // function headerPosFunction() {
+  //   if (window.pageYOffset > sticky) {
+  //     header.classList.add("sticky");
+  //   } else {
+  //     header.classList.remove("sticky");
+  //   }
+  // }
 
   function dropdownMenu() {
     var x = document.getElementById("dropdown-menu");
